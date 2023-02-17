c0003a774a6b32a4e850bef8deb562eadcb99194
Ben Munro <ben.munro2000@gmail.com>
2023-02-17 12:02:55 -0500

    Initial commit

---
+++ src/main/java/ca/carleton/AmazinBookStore/AmazinBookStoreApplication.java
@@ -0,0 +1,13 @@
+package ca.carleton.AmazinBookStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazinBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazinBookStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/carleton/AmazinBookStore/AmazinBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.carleton.AmazinBookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazinBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
