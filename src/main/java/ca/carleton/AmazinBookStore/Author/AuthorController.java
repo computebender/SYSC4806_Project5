4ae30e9b41b041e6d5747b4ca4273ab5bb8280e0
Ben Munro <ben.munro2000@gmail.com>
2023-02-28 18:45:03 -0500

    added all necessary endpoints

--- src/main/java/ca/carleton/AmazinBookStore/Author/AuthorController.java
+++ src/main/java/ca/carleton/AmazinBookStore/Author/AuthorController.java
@@ -1,5 +1,6 @@
 package ca.carleton.AmazinBookStore.Author;
 
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,5 +27,41 @@ public class AuthorController {
         Author savedAuthor = this.authorService.createAuthor(author);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedAuthor);
     }
+
+    @GetMapping("/{authorId}")
+    public ResponseEntity<Author> getAuthorById(@PathVariable long authorId) {
+        Author author;
+        try {
+            author = this.authorService.getAuthorById(authorId);
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(author);
+    }
+
+    @PutMapping("/{authorId}")
+    public ResponseEntity<Author> updateAuthorById(@PathVariable long authorId, @RequestBody Author partialAuthor) {
+        Author author;
+        try {
+            author = authorService.updateAuthor(authorId, partialAuthor);
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(author);
+    }
+
+    @DeleteMapping("/{authorId}")
+    public ResponseEntity<Void> deleteAuthor(@PathVariable Long authorId) {
+        try {
+            authorService.deleteAuthor(authorId);
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.noContent().build();
+
+    }
+
+
 
 }
--- src/main/java/ca/carleton/AmazinBookStore/Author/AuthorService.java
+++ src/main/java/ca/carleton/AmazinBookStore/Author/AuthorService.java
@@ -1,9 +1,11 @@
 package ca.carleton.AmazinBookStore.Author;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class AuthorService {
@@ -18,10 +20,46 @@ public class AuthorService {
     }
 
     public Author getAuthorById(Long id){
-        return this.authorRepository.getReferenceById(id);
+        Optional<Author> author = this.authorRepository.findById(id);
+
+        if(author.isEmpty()){
+            throw new ResourceNotFoundException("Author with ID " + id + " not found.");
+        }
+
+        return author.get();
     }
 
     public Author createAuthor(Author author){
         return this.authorRepository.save(author);
     }
+
+    public Author updateAuthor(Long id, Author partialAuthor) {
+        Optional<Author> optionalAuthor = this.authorRepository.findById(id);
+        if(optionalAuthor.isEmpty()){
+            throw new ResourceNotFoundException("Author with ID " + id + " not found.");
+        }
+        Author author = optionalAuthor.get();
+
+        if(Objects.nonNull(partialAuthor.getFirstName())){
+            author.setFirstName(partialAuthor.getFirstName());
+        }
+
+        if(Objects.nonNull(partialAuthor.getLastName())){
+            author.setFirstName(partialAuthor.getLastName());
+        }
+
+        this.authorRepository.save(author);
+
+        return author;
+    }
+
+    public void deleteAuthor(Long id) {
+        Optional<Author> optionalAuthor = this.authorRepository.findById(id);
+        if(optionalAuthor.isEmpty()){
+            throw new ResourceNotFoundException("Author with ID " + id + " not found.");
+        }
+        this.authorRepository.deleteById(optionalAuthor.get().getId());
+    }
+
+
 }
