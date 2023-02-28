33303de4e327a2e4c7fd80cf62c029b279877ce3
Ben Munro <ben.munro2000@gmail.com>
2023-02-28 18:57:46 -0500

    wip: began work on authorService

---
+++ src/main/resources/static/js/authorService.js
@@ -0,0 +1,59 @@
+const authorService = (function() {
+
+    function handleAjaxError(jqXHR, textStatus, errorThrown) {
+        console.log("AJAX error: " + textStatus, errorThrown);
+    }
+
+    return {
+        getAllAuthors: function(successCallback) {
+            $.ajax({
+                url: "/api/authors",
+                type: "GET",
+                success: successCallback,
+                error: handleAjaxError
+            });
+        },
+
+        createAuthor: function(author, successCallback) {
+            $.ajax({
+                url: "/api/authors",
+                type: "POST",
+                data: JSON.stringify(author),
+                contentType: "application/json",
+                success: successCallback,
+                error: handleAjaxError
+            });
+        },
+
+        getAuthorById: function(authorId, successCallback) {
+            $.ajax({
+                url: "/api/authors/" + authorId,
+                type: "GET",
+                success: successCallback,
+                error: handleAjaxError
+            });
+        },
+
+        updateAuthorById: function(authorId, partialAuthor, successCallback) {
+            $.ajax({
+                url: "/api/authors/" + authorId,
+                type: "PUT",
+                data: JSON.stringify(partialAuthor),
+                contentType: "application/json",
+                success: successCallback,
+                error: handleAjaxError
+            });
+        },
+
+        deleteAuthorById: function(authorId, successCallback) {
+            $.ajax({
+                url: "/api/authors/" + authorId,
+                type: "DELETE",
+                success: successCallback,
+                error: handleAjaxError
+            });
+        }
+
+    };
+
+})();
