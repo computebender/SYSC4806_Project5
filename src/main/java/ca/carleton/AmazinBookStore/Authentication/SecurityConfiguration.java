ae925d5b606a16b908f98749384e7726ee71d64f
Ben Munro <ben.munro2000@gmail.com>
2023-03-21 09:45:47 -0400

    removed unused code

--- src/main/java/ca/carleton/AmazinBookStore/Authentication/SecurityConfiguration.java
+++ src/main/java/ca/carleton/AmazinBookStore/Authentication/SecurityConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,15 +21,11 @@ import java.util.List;
 @EnableMethodSecurity
 public class SecurityConfiguration {
     @Autowired
-    private AuthenticationManagerBuilder authenticationManagerBuilder;
-
-    @Autowired
     JwtUserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http,
                                                    JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
-                                                   JwtUserDetailsService jwtUserDetailsService,
                                                    JwtRequestFilter jwtRequestFilter) throws Exception {
 
         http
@@ -48,15 +43,9 @@ public class SecurityConfiguration {
 
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
-        // Configure the AuthenticationManager to use your custom UserDetailsService
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
-
-        // Configure the password encoder
-        // Replace this with your desired password encoder, e.g., BCryptPasswordEncoder
         provider.setPasswordEncoder(new BCryptPasswordEncoder());
-
-        // Create and return the AuthenticationManager bean
         return new ProviderManager(List.of(provider));
     }
 }
