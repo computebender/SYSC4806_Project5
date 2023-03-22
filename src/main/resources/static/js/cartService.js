da2278424a1fe03bbd475f32634399002b7c7e28
Alex Cameron <72214882+alexcameron8@users.noreply.github.com>
2023-03-21 21:17:57 -0400

    WIP - Shopping Cart Front End

--- src/main/resources/static/js/cartService.js
+++ src/main/resources/static/js/cartService.js
@@ -2,53 +2,74 @@ const shoppingCartService = (function() {
     function handleAjaxError(jqXHR, textStatus, errorThrown) {
         console.log("AJAX error: " + textStatus, errorThrown);
     }
+
     return {
         // Define the function to send an AJAX request to create a new shopping cart
         createCart: function(userId, successCallback) {
-        $.ajax({
-            url: `/api/carts/${userId}`,
-            method: 'POST',
-            success: successCallback,
-            error: handleAjaxError
-        });
-    },
+            $.ajax({
+                url: `/api/carts/${userId}`,
+                method: 'POST',
+                success: successCallback,
+                error: handleAjaxError
+            });
+        },
 
         // Define the function to send an AJAX request to add an item to a shopping cart
         addItem: function(userId, listing, successCallback) {
-        $.ajax({
-            url: `/api/carts/${userId}/add-item`,
-            method: 'POST',
-            data: JSON.stringify(listing),
-            contentType: 'application/json',
-            success: successCallback,
-            error: handleAjaxError
-        });
-    },
+            $.ajax({
+                url: `/api/carts/${userId}/add-item`,
+                method: 'POST',
+                data: JSON.stringify(listing),
+                contentType: 'application/json',
+                success: successCallback,
+                error: handleAjaxError
+            });
+        },
         // Define the function to send an AJAX request to remove an item from a shopping cart
         removeItem: function(userId, cartItemId,successCallback) {
         $.ajax({
-            url: `/api/carts/${userId}/remove-item`,
-            method: 'DELETE',
-            data: JSON.stringify(cartItemId),
-            contentType: 'application/json',
-            success: successCallback,
-            error: handleAjaxError
-        });
-    },
+                url: `/api/carts/${userId}/remove-item`,
+                method: 'DELETE',
+                data: JSON.stringify(cartItemId),
+                contentType: 'application/json',
+                success: successCallback,
+                error: handleAjaxError
+            });
+        },
         // Define the function to send an AJAX request to get all shopping carts
-        getAllShoppingCarts: function() {
-        return $.ajax({
-            url: "/api/carts",
-            method: "GET",
+        getAllShoppingCarts: function(successCallback) {
+        $.ajax({
+                url: "/api/carts",
+                method: "GET",
+                success: successCallback,
+                error: handleAjaxError
+            });
+        },
+        // Define the function to send an AJAX request to get a specific shopping cart by user ID
+        getShoppingCartById: function(userId,successCallback) {
+        $.ajax({
+                method: "GET",
+                url: "/api/carts/" + userId,
+            });
+        },
+        // Define the function to send an AJAX request to clear a shopping cart
+        clearShoppingCart: function(userId,successCallback) {
+            $.ajax({
+                method: "POST",
+                url: `/api/carts/${userId}/clear`,
+                data: JSON.stringify(userId),
+                success: successCallback,
+                error: handleAjaxError
+            });
+        },
+        // Define the function to send an AJAX request to check out shopping cart
+        checkoutShoppingCart: function(userId,successCallback) {
+            $.ajax({
+            method: "POST",
+            url: `/api/carts/${userId}/checkout`,
+            data: JSON.stringify(userId),
             success: successCallback,
             error: handleAjaxError
-        });
-    },
-        // Define the function to send an AJAX request to get a specific shopping cart by user ID
-        getShoppingCartById: function(userId) {
-        return $.ajax({
-            method: "GET",
-            url: "/api/carts/" + userId,
         });
     }
 
