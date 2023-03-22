6448288ac3dc14832c41b9dd2e805eae4dc56987
Thomas <dunniganthomas@gmail.com>
2023-03-22 15:49:20 -0400

    Fixed Displaying Recommended book

--- src/main/resources/static/js/component/bookComponent.js
+++ src/main/resources/static/js/component/bookComponent.js
@@ -1,12 +1,4 @@
-$.fn.listingCard = function (title, author, price, coverUrl, listing, bookstore) {
-    function addToCart(listing) { // Change: Accept listing as an argument
-        userService.getCurrentUser(function (user) {
-            console.log(user)
-            cartService.addItem(user.shoppingCart.id, listing, function () {
-                notificationService.addSuccessNotification('Added to cart.');
-            });
-        });
-    }
+$.fn.bookCard = function (title, author,  coverUrl) {
 
     const defaultCover = 'https://blog.springshare.com/wp-content/uploads/2010/02/nc-md.gif';
     const cardStyle = {
@@ -38,22 +30,10 @@ $.fn.listingCard = function (title, author, price, coverUrl, listing, bookstore)
     const body = $('<div>').addClass('card-body');
     const titleEl = $('<h5>').addClass('card-title').text(title);
     const authorEl = $('<p>').addClass('card-text').text(author);
-    const priceEl = $('<p>').addClass('card-text').text('$' + price);
-    const soldByEl = $('<p>').addClass('card-text').text('Sold by: ' + bookstore.bookstoreName);
-    const buttonContainer = $('<div>').addClass('button-container').css(buttonContainerStyle);
-    const buttonEl = $('<a>').addClass('btn btn-primary').text('View Listing').attr('href', '/listing?listing=' + listing.id);
-    const addCartButton = $('<button>').addClass('btn btn-primary').text('Add to Cart').attr('id', 'add-cart-btn').click(function() {
-        addToCart(listing);
-    });
 
 
     body.append(titleEl);
     body.append(authorEl);
-    body.append(priceEl);
-    body.append(soldByEl);
-    buttonContainer.append(buttonEl);
-    buttonContainer.append(addCartButton);
-    body.append(buttonContainer);
     imageContainer.append(image);
     card.append(imageContainer);
     card.append(body);
